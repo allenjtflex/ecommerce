fef87a5e50698dbbc43186b44e56d4fda1b606cc
allen.chang <allen.chang@localhost.localdomain>
2017-03-17 21:04:41 +0800

    Product Variation

--- products/models.py
+++ products/models.py
@@ -7,6 +7,7 @@ class ProductQuerySet(models.query.QuerySet):
     def active(self):
         return self.filter(active=True)
 
+
 class ProductManager(models.Manager):
     def get_queryset(self):
         return ProductQuerySet(self.model, using=self.db)
@@ -15,12 +16,10 @@ class ProductManager(models.Manager):
         return self.get_queryset().active()
 
 
-
-
 class Product(models.Model):
     title = models.CharField(max_length=60, null=False, blank=False )
     discription = models.TextField(max_length=4000, null=True, blank=True)
-    price    = models.DecimalField(decimal_places=2,max_digits=20)
+    #price    = models.DecimalField(decimal_places=2,max_digits=20)
     active = models.BooleanField(default=True)
 
     objects = ProductManager()
@@ -30,3 +29,24 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"pk":self.pk})
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product)
+    title = models.CharField(max_length=120, null=False, blank=False)
+    discription = models.TextField(null=True, blank=True)
+    price    = models.DecimalField(decimal_places=2,max_digits=20)
+    sale_price    = models.DecimalField(decimal_places=2,max_digits=20, null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_price(self):
+        if sale_price is None:
+            return self.price
+        else:
+            return self.sale_price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
